60928cd678b8763931f88134edf89f97642effc7
taka.usu <liverpenguin1408@gmail.com>
2019-05-06 05:06:34 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
 class MessagesController < ApplicationController
+  #アクションが実行される前に、前もって実行するメソッドを指定
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  
   def index
     #モデルMessageより、データを全取得し、インスタンス変数へ格納
     @messages = Message.all
@@ -6,7 +9,6 @@ class MessagesController < ApplicationController
 
   def show
     #モデルMessageより、idに基づき、データを取得し、インスタンス変数へ格納
-    @message = Message.find(params[:id])
   end
 
   def new
@@ -29,15 +31,14 @@ class MessagesController < ApplicationController
   end
 
   def edit
-    @message = Message.find(params[:id])
   end
 
   def update
-    @message = Message.find(params[:id])
+    set_message
     
     if @message.update(message_params)
       flash[:success] = 'Messageは正常に更新されました'
-      redirect_to @message
+      redirect_to message_path(@message)
     else
       flash.now[:danger]
       render :edit
@@ -45,7 +46,6 @@ class MessagesController < ApplicationController
   end
 
   def destroy
-    @message = Message.find(params[:id])
     @message.destroy
     
     flash[:success] = 'Mesage は正常に削除されました'
@@ -55,6 +55,10 @@ class MessagesController < ApplicationController
   private
   
   # Strong Parameter
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
   def message_params
     #params.require(:message) → Mesageモデルのフォームから得られるデータ
     #.premit(:content) → Messageモデルのcontentを精査する。
