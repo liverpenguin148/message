59349c42a6b9a764c13751102aa7ecf7c2aaa468
taka.usu <liverpenguin1408@gmail.com>
2019-05-06 11:09:53 +0000

    add column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,31 +1,25 @@
 class MessagesController < ApplicationController
-  #アクションが実行される前に、前もって実行するメソッドを指定
   before_action :set_message, only: [:show, :edit, :update, :destroy]
-  
+
   def index
-    #モデルMessageより、データを全取得し、インスタンス変数へ格納
     @messages = Message.all
   end
 
   def show
-    #モデルMessageより、idに基づき、データを取得し、インスタンス変数へ格納
   end
 
   def new
-    #新規レコードのためのモデルインスタンスを作成する
     @message = Message.new
-    # @message = Message.new(content: 'sample') 
-    # → デフォルトでその値が入力された状態となる
   end
 
   def create
     @message = Message.new(message_params)
-    # @messageの保存
-    if @message.save 
-      flash[:success] = "Messageが正常に投稿されました"
+
+    if @message.save
+      flash[:success] = 'Message が正常に投稿されました'
       redirect_to @message
     else
-      flash.now[:danger] = "Messageが投稿されませんでした"
+      flash.now[:danger] = 'Message が投稿されませんでした'
       render :new
     end
   end
@@ -34,34 +28,29 @@ class MessagesController < ApplicationController
   end
 
   def update
-    set_message
-    
     if @message.update(message_params)
-      flash[:success] = 'Messageは正常に更新されました'
-      redirect_to message_path(@message)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
     else
-      flash.now[:danger]
+      flash.now[:danger] = 'Message は更新されませんでした'
       render :edit
     end
   end
 
   def destroy
     @message.destroy
-    
-    flash[:success] = 'Mesage は正常に削除されました'
+
+    flash[:success] = 'Message は正常に削除されました'
     redirect_to messages_url
   end
-  
+
   private
-  
-  # Strong Parameter
+
   def set_message
     @message = Message.find(params[:id])
   end
-  
+
   def message_params
-    #params.require(:message) → Mesageモデルのフォームから得られるデータ
-    #.premit(:content) → Messageモデルのcontentを精査する。
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :title)
   end
 end
